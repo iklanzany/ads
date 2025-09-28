268bb0d2b83ab6d4c496cf2d1c325e30da9a141f
iklanzany <iklanzany@gmail.com>
2025-09-28 08:21:12 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,5 @@
+var redirectUrl = "https://mitrabuylinkid.blogspot.com"; // link tujuan
+
+document.addEventListener("click", function () {
+  window.open(redirectUrl, "_blank");
+}, { once: true });
