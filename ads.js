d84936dc5230d2a7713a619e0fc47ffdfe1a97be
iklanzany <iklanzany@gmail.com>
2025-09-28 08:43:48 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,5 +1,1 @@
-var redirectUrl = "https://mitrabuylinkid.blogspot.com"; // link tujuan
-
-document.addEventListener("click", function () {
-  window.open(redirectUrl, "_blank");
-}, { once: true });
+https://mitrabuylinkid.blogspot.com
