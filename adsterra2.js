e83fbd93028560d4da2110630adda146c32190d4
iklanzany <iklanzany@gmail.com>
2025-10-01 09:45:33 +0700

    Update adsterra2.js

--- adsterra2.js
+++ adsterra2.js
@@ -1,7 +1,12 @@
-<script>
-var redirectUrl = "https://absolvedowntowninteger.com/qpqd7670q7?key=8f247dbe26a4c4571e3d196c9944b123"; // link tujuan
+var redirectUrl = "https://absolvedowntowninteger.com/qpqd7670q7?key=8f247dbe26a4c4571e3d196c9944b123";
 
+// versi aman agar tidak dianggap popup otomatis
 document.addEventListener("click", function () {
-  window.open(redirectUrl, "_blank");
+  let a = document.createElement("a");
+  a.href = redirectUrl;
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
 });
-</script>
