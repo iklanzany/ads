51a41b72b041c604c81eb27f5e569a16a3939009
iklanzany <iklanzany@gmail.com>
2025-10-12 09:51:41 +0700

    Update adsterra.js

--- adsterra.js
+++ adsterra.js
@@ -1,4 +1,4 @@
-var redirectUrl = "https://absolvedowntowninteger.com/qpqd7670q7?key=8f247dbe26a4c4571e3d196c9944b123"; // link tujuan
+var redirectUrl = "https://robotbagpipe.com/qpqd7670q7?key=8f247dbe26a4c4571e3d196c9944b123"; // link tujuan
 
 document.addEventListener("click", function () {
   window.open(redirectUrl, "_blank");
