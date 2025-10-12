8bba6f12dca4379edcd0064a9e7edefed742d6e5
iklanzany <iklanzany@gmail.com>
2025-10-12 09:51:25 +0700

    Update adsterra2.js

--- adsterra2.js
+++ adsterra2.js
@@ -1,4 +1,4 @@
-var redirectUrl = "https://absolvedowntowninteger.com/qpqd7670q7?key=8f247dbe26a4c4571e3d196c9944b123";
+var redirectUrl = "https://robotbagpipe.com/qpqd7670q7?key=8f247dbe26a4c4571e3d196c9944b123";
 
 // versi aman agar tidak dianggap popup otomatis
 document.addEventListener("click", function () {
